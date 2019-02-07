a875274417217c9974968bd5ab1be0381ff292a1
Nicholas VanKuren <nwvankuren@gmail.com>
2019-02-07 15:44:47 -0600

    Inital commit for gwplotting

---
+++ R/file_loading_functions.R
@@ -0,0 +1,270 @@
+# Loading functions
+
+#' Load a GEMMA association results file.
+#'
+#' This function will read a (possibly gzipped) GEMMA results file (.assoc.txt)
+#' and return a tibble containing just the scaffold, position, and statistic.
+#'
+#' @param file GEMMA file (.assoc.txt). Can be gzipped.
+#' @param pval Statistic column to read. This must match the column name
+#'     exactly and can be one of the following: p_wald, p_lrt, or p_score.
+#'
+#' @return A three-column tibble containing scaffold, position, and stat
+#' @export
+#'
+#' @examples
+#' a <- system.file("extdata", "test.gemma_gwas.txt.gz",
+#'                  package = "gwplotting")
+#'
+#' b <- load_gemma_gwas( a, pval = 'p_wald' )
+#' b
+#'
+load_gemma_gwas <- function( file, pval = 'p_wald' ){
+
+  pp <- c( 'p_wald', 'p_lrt', 'p_score')
+
+  if( pval %in% pp ){
+
+    tf <- readr::read_table2( file, col_names = T )
+    tf <- dplyr::select( tf, chr, ps, pval )
+    colnames( tf ) <- c( 'scaf', 'ps', 'stat' )
+    tf <- dplyr::mutate( tf, scaf = stringr::str_replace( scaf, "[|]size[:digit:]*$", "") )
+
+    # Add a false chr column with a number for each chromosome
+    scaf_order <- tibble::tibble( scaf = rle( tf$scaf )$values ,
+                          num = as.numeric(seq( 1:length(unique( tf$scaf )))))
+
+    tf$chr <- scaf_order$num[ match( tf$scaf, scaf_order$scaf ) ]
+
+    return( tf )
+
+  } else {
+
+    stop("pval can only take values p_wald, p_lrt, or p_score")
+
+  }
+}
+
+#' Load a VCFtools results file.
+#'
+#' This function will read a (possibly gzipped) VCFtools results file
+#' from windowed Fst analysis (.windowed.weir.fst), nucleotide diversity
+#' analysis (.windowed.pi), or Tajima's D (.Tajima.D) and return a tibble
+#' containing just the scaffold, position, and statistic.
+#'
+#' @param file VCFtools results file. Can be gzipped.
+#' @param stat Name of the column to use as statistic. This will be mean_fst,
+#'     weighted_fst, pi, tajimas_d, ...
+#' @param position Coordinate to use as position: start, end, or midpoint
+#'
+#' @return A three-column tibble containing scaffold, bin position, and
+#'     statistic
+#' @export
+#'
+#' @examples
+#' a <- system.file("extdata", "test.vcftools_pi.txt.gz",
+#'                  package = "gwplotting")
+#' b <- load_vcftools_stats( a, stat = 'pi' )
+#' b
+#'
+load_vcftools_stats <- function( file, stat = 'mean_fst',
+                                 position = 'midpoint' ){
+  # Correct column name
+  if( stat == 'mean' ){
+    stat <- 'MEAN_FST'
+  } else if( stat == 'weighted_fst' ){
+    stat <- 'WEIGHTED_FST'
+  } else if( stat == 'pi' ) {
+    stat <- 'PI'
+  } else if( stat == 'tajimas_d'){
+    stat <- 'TajimaD'
+  } else {
+    stop("stat can only take certain values")
+  }
+
+  # Read in table
+  tf <- readr::read_table2( file, col_names = T )
+  tf <- dplyr::mutate( tf, chr = stringr::str_replace( CHROM, "[|]size[:digit:]*$", "") )
+
+  if( 'BIN_START' %in% colnames( tf ) ){
+
+    if( position == "midpoint" ){
+      tf <- dplyr::mutate( tf, mp = ((BIN_END + BIN_START) / 2) )
+      tf <- dplyr::select( tf, CHROM, mp, stat )
+    } else if( position == "start" ){
+      tf <- dplyr::select( tf, CHROM, BIN_START, stat )
+    } else {
+      tf <- dplyr::select( tf, CHROM, BIN_END, stat )
+    }
+
+  } else {
+
+    if( position == "start" ){
+      warning("No BIN_START in input file, setting position to BIN_END")
+    }
+
+    tf <- dplyr::select( tf, CHROM, BIN_END, stat )
+
+  }
+
+  colnames(tf) <- c( 'scaf', 'ps', 'stat' )
+
+  # Add a false chr column with a number for each chromosome, in case you don't
+  # reorder you scaffolds in any way.
+  scaf_order <- tibble::tibble( scaf = rle( tf$scaf )$values ,
+                                num = as.numeric(seq( 1:length(unique( tf$scaf )))))
+
+  tf$chr <- scaf_order$num[ match( tf$scaf, scaf_order$scaf ) ]
+
+  return( tf )
+
+}
+
+#' Load a PLINK association results file.
+#'
+#' This function will read a (possibly gzipped) PLINK results file (.assoc.txt)
+#' and return a tibble containing just the scaffold, position, and statistic.
+#'
+#' @param file PLINK association file
+#'
+#' @return A three-column tibble containing scaffold, bin midpoint, and
+#'     statistic value.
+#' @export
+#'
+#' @examples
+#' a <- system.file("extdata", "test.plink_gwas.qassoc.gz",
+#'                  package = "gwplotting")
+#' b <- load_plink_gwas( a )
+#' b
+load_plink_gwas <- function( file ){
+
+  tf <- readr::read_table2( file, col_names = T )
+  tf <- dplyr::mutate( tf, CHR = stringr::str_replace( CHR, "[|]size[:digit:]*$", "") )
+
+  if( colnames( tf )[3] == "UNADJ" ){ # It has been adjusted
+    # tf <- select( tf, CHR, BP, FDR_BH )
+    stop("Cannot handle adjusted p-values yet. You can paste your unadjusted
+         and ajusted files together and cut the proper columns if you want -
+         you will need CHR, BP, and P (FDR_BH if you want the adjusted values)")
+  } else { # It hasn't been adjusted
+    tf <- dplyr::select( tf, CHR, BP, P )
+  }
+
+  colnames( tf ) <- c( 'scaf', 'ps', 'stat' )
+
+  # Add a false chr column with a number for each chromosome
+  scaf_order <- tibble::tibble( scaf = rle( tf$scaf )$values ,
+                        num = as.numeric(seq( 1:length(unique( tf$scaf )))))
+
+  tf$chr <- scaf_order$num[ match( tf$scaf, scaf_order$scaf ) ]
+
+  return( tf )
+
+}
+
+#' Load a results file from Simon Martin's ABBABABAwindows.py script.
+#'
+#' This function was developed strictly following his GitHub description of the
+#' output file. The columns in this output file are: scaffold, start, end,
+#' sites, sitesUsed, ABBA, BABA, D, fd, fdM. You can choose to plot any of the
+#' statistics, and filter windows by the number of sites used.
+#'
+#' @param file Input file name
+#' @param stat Statistic(s) to keep. This can be a vector of column names.
+#' @param position Use the start, end, or midpoint as the position?
+#' @param min_sites Minimum number of sitesUsed required for window inclusion.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' a <- system.file("extdata", "test.abbababa.csv.gz",
+#'                  package = "gwplotting")
+#' b <- load_abbababa( a, stat = 'D' )
+#' b
+load_abbababa <- function( file, stat = 'D', position = 'midpoint',
+                           min_sites = 0 ){
+
+  tf <- readr::read_csv( file, col_names = T )
+  tf <- dplyr::mutate( tf, scaffold = stringr::str_replace( scaffold, "[|]size[:digit:]*$", "") )
+
+  # Filter out windows with small numbers of sites
+  tf <- dplyr::filter( tf, sitesUsed >= min_sites )
+
+  # Position
+  if( position == 'midpoint' ){
+    if( 'mid' %in% colnames(tf) ){
+      tf <-dplyr::select( tf, scaffold, mid, tidyselect::one_of( stat ))
+    } else{
+      tf <- dplyr::mutate( tf, ps = ((end + start) / 2) )
+      tf <- dplyr::select( tf, scaffold, ps, tidyselect::one_of(stat))
+    }
+  } else if( position == 'start' ){
+    tf <- dplyr::select( tf, scaffold, start, tidyselect::one_of(stat) )
+  } else if( position == 'end' | ! position %in% c('midpoint', 'start', 'end') ){
+    tf <- dplyr::select( tf, scaffold, end, tidyselect::one_of(stat) )
+  }
+
+  # Standardize column names
+  colnames( tf )[1:2] <- c('scaf','ps')
+  if( length( stat ) == 1 ){ colnames(tf)[3] <- 'stat' }
+
+  # Add a false chr column with a number for each chromosome
+  scaf_order <- tibble::tibble( scaf = rle( tf$scaf )$values ,
+                        num = as.numeric(seq( 1:length(unique( tf$scaf )))))
+
+  tf$chr <- scaf_order$num[ match( tf$scaf, scaf_order$scaf ) ]
+
+  return( tf )
+
+}
+
+
+#' Load a plink LD file
+#'
+#' This function loads a file containing pairwise LD estimates generated by
+#' plink. A typical command to generate this file would be:
+#' \emph{plink --bfile input --allow-extra-chr --r2 inter-chr gz \
+#' --ld-window-r2 0.0 --out test.plink --chr scaffold_1000}
+#' This will only keep SNPs on the same chromosome, because it assumes that you
+#' do not know the distance between different scaffolds, even those located to
+#' the same chromosome according to reordering information.
+#'
+#' @param file Name of a file containing LD values calculated using
+#' @param max_dist Maximum allowed distance between two SNPs, in bp
+#' @param keep_ids Keep SNP IDs in returned tibble? T / F
+#' @param stat Which statistic to keep? r2, d, and/or dp (D' and D'-signed)
+#'
+#' @return A tibble with pairwise LD measures
+#' @export
+#'
+#' @examples
+#' a <- system.file("extdata", "test.plink.ld.gz", package = "gwplotting" )
+#' b <- load_plink_ld( a )
+#' b
+load_plink_ld <- function( file, max_dist = 50000, keep_ids = FALSE,
+                           stat = c('r2', 'dp') ){
+
+  # cols to keep at the end
+  keep <- c('scaf','ps','dist',stat)
+
+  if( keep_ids ){ keep <- c( keep, 'snpA' ) }
+
+  tf <- readr::read_table2( file, col_names = T ) %>% select( -X9 )
+
+  cnames <- c('scaf','ps','snpA','scaf2','ps2','snpB','r2','dp')
+  colnames(tf) <- cnames[ 1:ncol( tf ) ]
+
+  tf <- dplyr::filter( tf, scaf == scaf2 ) %>%
+    dplyr::mutate( dist = ps2 - ps ) %>%
+    dplyr::select( tidyselect::one_of( keep ) )
+
+  # Add a false chr column with a number for each chromosome
+  scaf_order <- tibble::tibble( scaf = rle( tf$scaf )$values ,
+                                num = as.numeric(seq( 1:length(unique( tf$scaf )))))
+
+  tf$chr <- scaf_order$num[ match( tf$scaf, scaf_order$scaf ) ]
+
+  return( tf )
+
+}
---
+++ R/reordering_functions.R
@@ -0,0 +1,219 @@
+#' Reorder scaffolds based on assignment to chromosomes.
+#'
+#' This function will reorder the results of a genome-wide statistical
+#' analysis based on the contents of a file that assigns each scaffold to a
+#' reference genome assembly. The assignments can be generated using, e.g.
+#' OrderScaffoldsByBlatingProteins.pl, but must be in the following format:
+#'
+#' The scaffold_info file can be generated using UCSCtools' faToTwoBit and
+#' twoBitInfo.
+#'
+#' @param input A three-column tibble containing (at minimum) scaffold, ps, and
+#'     stat as the first three columns.
+#' @param assignments Name of the file containing the reordering information.
+#'
+#' @return Returns the input tibble with a new column, chr, detailing which
+#'     reference genome chromosomes the draft genome scaffolds map to. Only
+#'     input rows that reside on mapped scaffolds are included.
+#' @export
+#'
+#' @examples
+#' a1 <- system.file("extdata", "test.gemma_gwas.txt.gz",
+#'                   package = "gwplotting")
+#'
+#' a2 <- system.file("extdata", "test.reordering.txt.gz",
+#'                   package = "gwplotting")
+#'
+#' b <- load_gemma_gwas( a1, pval = 'p_wald' )
+#' b <- reorder_scaffolds( b, a2 )
+#' b
+reorder_scaffolds <- function( input , assignments ){
+
+  reordered <- readr::read_table2( assignments , col_names = T  )
+  #`#scaf`     scafLen scafMin scafMax chr    chrMin  chrMax strand num_proteins median_pos
+  #<chr>         <int>   <int>   <int> <chr>   <int>   <int> <chr>         <int>
+  #1 scaffold49  1564542   53542  901564 chr7  6339484 6621196 +                19
+  #2 scaffold460  191485   28352  184843 chr2  3270890 3397095 +                 8
+  #3 scaffold580  104787   14511   68652 chr3  8062310 8083970 +                 3
+
+  colnames( reordered )[1] <- 'scaf'
+
+  # Strip 'chr' from chromosome names, if they're there. This simplifies -
+  # will plot in numerically increasing chromosome order.
+  if( grepl( "chr", reordered[1,5] ) ){
+    reordered$chr <- as.numeric( unlist( purrr::map( reordered$chr,
+                                         stringr::str_replace, "chr", "")))
+  }
+
+  # Also strip platanus scaffold sizes if they're there
+  reordered <- dplyr::mutate( reordered,
+                 scaf = stringr::str_replace( scaf, "[|]size[:digit:]*$", "") )
+
+  # Make sure it's in the proper order. If it's numeric, then fine. It should order
+  # properly for Hmel chromosomes, too, because it treats them as strings and the
+  # Hmel genome is, e.g. Hmel201001o and Hmel213001o. Check, though.
+  reordered <- reordered[ order( reordered$chr, reordered$median_pos ), ]
+
+  # Keep only those SNPs on reordered scaffolds
+  input <- dplyr::filter( input, scaf %in% reordered$scaf )
+
+  # Some scaffolds hit the reference on the reverse strand
+  for_rev <- as.character( reordered$scaf[ reordered$strand == "-" &
+                                             ! is.na( reordered$strand )] )
+
+  for( rscaf in 1:length(for_rev) ){
+
+    # Get the length of the scaffold that needs to be reversed, get the
+    # positions
+    scaf_len <- reordered$scafLen[ reordered$scaf == for_rev[rscaf] ]
+    pos1 <- input$ps[ input$scaf == for_rev[rscaf] ]
+
+    # The new position is length - pos1. Check that there are sites on that
+    # scaffold first.
+    if( length( pos1 ) > 0 ){
+      pos2 <- scaf_len - pos1
+      input$ps[ input$scaf == for_rev[rscaf] ] <- pos2
+    }
+
+  }
+
+  # Actually do the reordering - by chrom then by position. Add a new column to
+  # input that tells you which reference scaffold / chromosome the scaffolds
+  # match
+  input$chr <- reordered$chr[ match( input$scaf, reordered$scaf ) ]
+  input$mpos <- reordered$median_pos[ match( input$scaf, reordered$scaf ) ]
+
+  # Sanity check
+  input <- filter(input, ! is.na( chr ) )
+
+  # Order by chromosome, then relative position on the chrom, then position on
+  # the scaffold.
+  input <- input[ order( input$chr,
+                         input$mpos,
+                         input$ps ), ]
+
+  input <- dplyr::select( input, -mpos )
+
+  return( input )
+
+}
+
+#' Reorder scaffolds based on their size.
+#'
+#'This function will order a genome stats tibble according to increasing
+#'scaffold size.
+#'
+#' @param input A three-column tibble containing (at minimum) scaffold, ps, and
+#'     stat as the first three columns.
+#' @param scaffold_lengths Name of the two-column tab-delimited file containing
+#'     scaffold name and length.
+#'
+#' @return A four-column tibble with scaffolds ordered in increasing size.
+#' @export
+#'
+#' @examples
+#' a1 <- system.file("extdata", "test.gemma_gwas.txt.gz",
+#'                  package = "gwplotting")
+#'
+#' a2 <- system.file("extdata", "test.chromSizes.txt.gz",
+#'                   package = "gwplotting" )
+#'
+#' b <- load_gemma_gwas( a1, pval = 'p_wald' )
+#' c <- reorder_by_scaf_len( b, a2 )
+#' c
+reorder_by_scaf_len <- function( input, scaffold_lengths ){
+
+  # Get the scaffolds and lengths
+  lens <- readr::read_table2( scaffold_lengths, col_names = F )
+  colnames( lens ) <- c( 'scaf', 'length' )
+  lens <- dplyr::mutate( lens,
+                  scaf = stringr::str_replace( scaf, "[|]size[:digit:]*$", ""))
+  lens <- lens[ order( lens$length, decreasing = T ) ,]
+
+  # Assign "chromosome" numbers according to size order
+  input$chr <- rownames( lens )[ match( input$scaf, lens$scaf ) ]
+  input <- input[ order( as.numeric( input$chr ),
+                         as.numeric( input$ps ) ), ]
+
+  return( input )
+
+}
+
+
+#' Assign cumulative positions to postions split by scaffolds.
+#'
+#'This function will add a new column to your tibble, bp_cum with the cumulative
+#'sum of positions in the ps column. This makes it so that each row in the
+#'tibble can be plotted sequentially.
+#'
+#' @param input A tibble, usually with the columns scaf (scaffold), ps
+#'     (position), and stat (statistic) at the minimum. This function operates
+#'     on the ps column.
+#' @param buffer Extra amount to add between scafs scaffolds or chromosomes.
+#' @param after Where to add buffer, either after scaffolds or chromosomes.
+#' @param scaffold_lengths Name of the two-column tab-delimited file containing
+#'     scaffold name and length.
+#'
+#' @return A reordered tibble.
+#' @export
+#'
+#' @examples
+#' a1 <- system.file("extdata", "test.gemma_gwas.txt.gz",
+#'                   package = "gwplotting" )
+#'
+#' a2 <- system.file("extdata", "test.chromSizes.txt.gz",
+#'                   package = "gwplotting" )
+#'
+#' b <- load_gemma_gwas( a1, pval = 'p_wald' )
+#' b <- get_cumulative_positions( input = b, scaffold_lengths = a2,
+#' buffer=10000, after = 'scaffolds' )
+#' b
+get_cumulative_positions <- function( input, scaffold_lengths, buffer = 0,
+                                      after = 'scaffolds' ){
+
+  if( after == "scaffolds" ){
+    after <- 'scaf_num'
+  } else if( after == "chromosomes" ){
+    after <- 'chr'
+  } else {
+    stop("after must be set to either scaffolds or chromosomes")
+  }
+
+  # Each subsequent scaffold gets the cumulative length of previous scaffolds
+  # added to each position. Have to make totLens separately because you can't
+  # put an order(match()) command in a pipe.
+
+  # Get lengths of each scaffold, then get cumulative positions
+  tot_lens <- readr::read_table2( scaffold_lengths, col_names = F )
+  colnames( tot_lens ) <- c( 'scaf', 'length' )
+  tot_lens <- dplyr::mutate( tot_lens,
+              scaf = stringr::str_replace( scaf, "[|]size[:digit:]*$", "") )
+
+  # Get order of scaffolds in the input file
+  scaf_order <- tibble::tibble( scaf = rle( input$scaf )$values ,
+                        num = as.numeric( seq(1:length( unique( input$scaf )))))
+
+  # For keeping order in input
+  input$scaf_num <- scaf_order$num[ match( input$scaf, scaf_order$scaf ) ]
+
+  tot_lens <- dplyr::filter( tot_lens, tot_lens$scaf %in% scaf_order$scaf )
+  tot_lens <- tot_lens[ match( scaf_order$scaf, tot_lens$scaf ), ]
+
+  # Actually shift the positions
+  input <- tot_lens %>%
+
+    # Get cumulative chromosome sizes
+    dplyr::mutate( cum_sum = cumsum( length ) - length ) %>%
+
+    # Add the matching running total to a new column in gwas
+    dplyr::left_join( input, . , by = c("scaf" = "scaf")) %>%
+
+    # Add a new column of ps + cumSum + a buffer
+    dplyr::mutate( bp_cum = ps + cum_sum + buffer * (chr - 1 ))
+
+
+  input <- dplyr::select( input, scaf, ps, stat, chr, bp_cum )
+
+  return( input )
+
+}
